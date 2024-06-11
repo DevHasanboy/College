97b95fbec55de47df55e4b6715c7e3454f2b6be2
Jorayev Hasanboy <hasnboy@gmail.com>
2024-06-11 12:38:06 +0200

    update

--- src/main/java/com/example/college/impl/mapper/CourseStudentMapper.java
+++ src/main/java/com/example/college/impl/mapper/CourseStudentMapper.java
@@ -8,24 +8,17 @@ import org.springframework.stereotype.Component;
 @Mapper(componentModel = "spring")
 public abstract class CourseStudentMapper {
 
-    @Mapping(target = "id",ignore = true)
-    @Mapping(target = "createdAt",ignore = true)
-
-
-    @Mapping(target = "updatedAt",ignore = true)
-
-    @Mapping(target = "deletedAt",ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
     public abstract CourseStudent toEntity(CourseStudentDto dto);
 
-    public  abstract CourseStudentDto toDto(CourseStudent courseStudent);
+    public abstract CourseStudentDto toDto(CourseStudent courseStudent);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "createdAt",ignore = true)
-
-    @Mapping(target = "updatedAt",ignore = true)
-
-    @Mapping(target = "deletedAt",ignore = true)
-
-
-    public abstract void toUpdate(@MappingTarget CourseStudent courseStudent, CourseStudentDto dto) ;
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    public abstract void toUpdate(@MappingTarget CourseStudent courseStudent, CourseStudentDto dto);
 }
